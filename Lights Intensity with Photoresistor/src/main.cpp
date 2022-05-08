187d4b00334c6a3fee63ae2d741388d8f5c303c5
Gilles Zunino <gzunino@hotmail.com>
2022-05-07 22:18:59 -0700

    [Lights Intensity with Photoresistor] Do not update LED bar display when the current reading is the same as the previous reading

--- Lights Intensity with Photoresistor/src/main.cpp
+++ Lights Intensity with Photoresistor/src/main.cpp
@@ -111,45 +111,50 @@ void singleLedForwardLoop()
 void displayLightIntensity()
 {
   // Read photo resistor value
-  int reading  = analogRead(PHOTORESISTOR_INPUT_PIN);
+  static int previousReading = 0;
+  int reading = analogRead(PHOTORESISTOR_INPUT_PIN);
+  if (reading != previousReading)
+  {
+    previousReading = reading;
 
-  // === Photoresistor KLS6-3537 ====================================================
-  // * 0  lux (dark)  -> 2M Ohm              [Measured 3.2 M Ohm]
-  // * 10 lux (light) -> 18K Ohm to 50 K Ohm [Measured 200 Ohm when illuminated]
-  // 
-  //  Vcc --- PhotoResistor ---- 10K Ohm ---- GND
-  //                          |
-  //                        Pin A0
-  //
-  // We measure across a 10K Ohm resistor. The photoresistor is supplied with 5V
-  // Using a voltage divider: VAO = (5V * 10KOhm) / (10K Ohm + RPhotoresistor)
-  // ================================================================================
+    // === Photoresistor KLS6-3537 ====================================================
+    // * 0  lux (dark)  -> 2M Ohm              [Measured 3.2 M Ohm]
+    // * 10 lux (light) -> 18K Ohm to 50 K Ohm [Measured 200 Ohm when illuminated]
+    // 
+    //  Vcc --- Photoresistor ---- 10K Ohm ---- GND
+    //                          |
+    //                        Pin A0
+    //
+    // We measure across a 10K Ohm resistor. The photoresistor is supplied with 5V
+    // Using a voltage divider: VAO = (5V * 10KOhm) / (10K Ohm + RPhotoresistor)
+    // ================================================================================
 
-  // Arduino Analog to Digital resolution is 10 bits
-  const int MaxValueAnalogRead = 1023;
-
-#if DEBUG
-  // Calculate the observed voltage
-  const float Vcc = 4.5;
-  float voltage = (Vcc * reading) / MaxValueAnalogRead;
-#endif
-
-  // Calculate the percentage of the LED Bar to light up - Dark: 0% and Full lit: 100%
-  uint8_t lightIntensityInPercent = (100.0 * reading) / MaxValueAnalogRead;
-  ledBarDriver.displayPercent(lightIntensityInPercent);
+    // Arduino Analog to Digital resolution is 10 bits
+    const int MaxValueAnalogRead = 1023;
 
 #if DEBUG
-  String serialOutput;
-
-  serialOutput.concat("R:");
-  serialOutput.concat(reading);
-  serialOutput.concat("-V:");
-  serialOutput.concat(voltage);
-  serialOutput.concat("-P:");
-  serialOutput.concat(lightIntensityInPercent);
-
-  Serial.println(serialOutput);
+    // Calculate the observed voltage
+    const float Vcc = 4.5;
+    float voltage = (Vcc * reading) / MaxValueAnalogRead;
 #endif
+
+    // Calculate the percentage of the LED Bar to light up - Dark: 0% and Full lit: 100%
+    uint8_t lightIntensityInPercent = (100.0 * reading) / MaxValueAnalogRead;
+    ledBarDriver.displayPercent(lightIntensityInPercent);
+
+#if DEBUG
+    String serialOutput;
+
+    serialOutput.concat("R:");
+    serialOutput.concat(reading);
+    serialOutput.concat("-V:");
+    serialOutput.concat(voltage);
+    serialOutput.concat("-P:");
+    serialOutput.concat(lightIntensityInPercent);
+
+    Serial.println(serialOutput);
+#endif
+  }
 
   // Ensure we only poll slower than the fastest refresh = Our photoresistor takes about 30ms to react to change
   const unsigned long timeBetweenReadingInMilliseconds = 50;
