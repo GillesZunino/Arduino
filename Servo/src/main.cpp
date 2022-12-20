43e976819b37eff4c8b7f005bb4f59279b8533b8
Gilles Zunino <gzunino@hotmail.com>
2022-12-20 15:51:13 -0800

    [Servo] Use common RotaryEncoder library

--- Servo/src/main.cpp
+++ Servo/src/main.cpp
@@ -1,7 +1,7 @@
-#include <util/atomic.h>
 #include <Arduino.h>
 #include <Servo.h>
 
+#include <RotaryEncoder.h>
 
 
 #if DEBUG
@@ -10,83 +10,11 @@
 #endif
 
 
-// Rotary Encoder --------------------------------------------------------------------------------------------------------------------------------------
-
 #define PIN_ENCODER_SWITCH 2
 #define PIN_ENCODER_CLK 3
 #define PIN_ENCODER_DT 4
 
-
-typedef enum {
-  Unknown = 0,
-  Clockwise = 1,
-  CounterClockwise = 2 
-} TurnDirection;
-
-
-volatile TurnDirection direction = TurnDirection::Unknown;
-volatile int rotaryEncoderPosition = 0;
-
-volatile byte rotaryEncoderValueHasChanged;
-
-void EncoderCLKChangedHandler()
-{
-  static int lastCLKValue = HIGH;
-
-  int CLKValue = digitalRead(PIN_ENCODER_CLK);
-  int DTValue = digitalRead(PIN_ENCODER_DT);
-
-  //
-  // For evey single encoder 'click', CLK will first go HIGH to LOW and then from LOW to HIGH
-  // We need to react to one transition only or we will double count 'clicks'
-  // We choose to the HIGH to LOW transition since CLK is HIGH when the encoder is idle and switches to LOW when the encoder is manipulated
-  //
-  if (CLKValue != lastCLKValue) {
-    if (CLKValue == LOW) {
-      rotaryEncoderValueHasChanged = true;
-
-      if (DTValue != CLKValue) {
-        rotaryEncoderPosition--;
-        direction = TurnDirection::CounterClockwise;
-      } else {
-        rotaryEncoderPosition++;
-        direction = TurnDirection::Clockwise;
-      }
-    }
-
-    lastCLKValue = CLKValue;
-  }
-}
-
-void EncoderSwitchChangedHandler() {
-  static int lastSwitchValue = HIGH;
-
-  int SwitchValue = digitalRead(PIN_ENCODER_SWITCH);
-  if (SwitchValue != lastSwitchValue) {
-    // Switch is active LOW
-    if (SwitchValue == LOW) {
-      direction = TurnDirection::Unknown;
-      rotaryEncoderPosition = 0;
-      rotaryEncoderValueHasChanged = true;
-    }
-
-    lastSwitchValue = SwitchValue;
-  }
-}
-
-void setupRotaryEncoder() {
-  pinMode(PIN_ENCODER_CLK, INPUT);
-  pinMode(PIN_ENCODER_DT, INPUT);
-  pinMode(PIN_ENCODER_SWITCH, INPUT_PULLUP);
-
-  rotaryEncoderValueHasChanged = false;
-
-  // It appears digitalPinToInterrupt() seems to only be able to convert pins 1, 2, 3
-  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_CLK), EncoderCLKChangedHandler, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_SWITCH), EncoderSwitchChangedHandler, CHANGE);
-}
-
-// ----------------------------------------------------------------------------------------------------------------------------------------------------
+RotaryEncoder rotaryEncoder(PIN_ENCODER_CLK, PIN_ENCODER_DT, PIN_ENCODER_SWITCH);
 
 
 //
@@ -111,7 +39,7 @@ void setup() {
 #endif
 
   // Initialize rotary encoder
-  setupRotaryEncoder();
+  rotaryEncoder.initialize();
 
   // Initialize the servo library and move the servo to center position (90°)
   servo.attach(SERVO_PWM_PIN);
@@ -119,24 +47,18 @@ void setup() {
 }
 
 void loop() {
-  int currentRotaryEncoderValue;
-  byte hasNewRotaryEncoderValue = false;
+  int currentEncoderValue;
+  RotaryEncoder::TurnDirection currentEncoderDirection;
 
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    hasNewRotaryEncoderValue = rotaryEncoderValueHasChanged;
-    currentRotaryEncoderValue = rotaryEncoderPosition;
-    rotaryEncoderValueHasChanged = false;
-  }
-
-  if (hasNewRotaryEncoderValue) {
+  if (rotaryEncoder.DetectRotaryEncoderChanges(currentEncoderValue, currentEncoderDirection)) {
     //
     // The servo we use can go from 0° to 180° - We choose 90° to be the 'middle'
     // * 0°  : Index is located on the right of the servo at 90° (servo wire oriented up)
     // * 90° : Index is located straight up (servo wire oriented up)
     // * 180 : Index is located on the left of the servo at 90° (servo wire oriented up)
     //
-    bool clockWise = currentRotaryEncoderValue > 0;
-    int clampedRotaryEncoderAngle = abs(currentRotaryEncoderValue) <= SERVO_INITIAL_ANGLE ? abs(currentRotaryEncoderValue) : SERVO_INITIAL_ANGLE;
+    bool clockWise = currentEncoderValue > 0;
+    int clampedRotaryEncoderAngle = abs(currentEncoderValue) <= SERVO_INITIAL_ANGLE ? abs(currentEncoderValue) : SERVO_INITIAL_ANGLE;
     int servoAngle = clockWise ? SERVO_INITIAL_ANGLE - clampedRotaryEncoderAngle : SERVO_INITIAL_ANGLE + clampedRotaryEncoderAngle;
     servo.write(servoAngle);
   }
