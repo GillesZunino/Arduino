928b0c7fc7038ff32478fd753a11e70c415125ba
Gilles Zunino <gzunino@hotmail.com>
2022-12-22 17:09:07 -0800

    Add MAX7219 library

---
+++ Personal-Libraries/MAX7219 LED Matrix/MAX7219LEDMatrix.cpp
@@ -0,0 +1,111 @@
+#include <MAX7219LEDMatrix.h>
+
+
+MAX7219LEDMatrix::MAX7219LEDMatrix(int din, int cs, int clk) {
+    dinPin = din;
+    csPin = cs;
+    clkPin = clk;
+}
+
+void MAX7219LEDMatrix::initialize() {
+    pinMode(dinPin, OUTPUT);
+    pinMode(csPin, OUTPUT);
+    pinMode(clkPin, OUTPUT);
+
+    // Configure the MAX7219 chip
+    configure();
+}
+
+void MAX7219LEDMatrix::shutdown(bool on) {
+    if ( on) {
+        // Mode: SHUTDOWN
+        spiSendCommand(Command_Shutdown, 0x00);
+    } else {
+        // Mode: NORMAL
+        spiSendCommand(Command_Shutdown, 0x01);
+    }
+}
+
+void MAX7219LEDMatrix::displayTest(bool on) {
+    if (on) {
+        spiSendCommand(Command_TestMode, 0x01);
+    } else {
+        spiSendCommand(Command_TestMode, 0x00);
+    }
+}
+
+void MAX7219LEDMatrix::setBrightness(uint8_t level) {
+    if ((level >= 0x00) && (level <= 0x0F)) {
+        spiSendCommand(Command_Intensity, level);
+    }
+}
+
+void MAX7219LEDMatrix::setRow(int row, uint8_t value) {
+    if ((row >= 0) && (row <= 7)) {
+        currentRows[row] = value;
+        spiSendCommand(row + 1, value);
+    }
+}
+
+void MAX7219LEDMatrix::setColumn(int column, uint8_t value) {
+    if ((column >= 0) && (column <= 7)) {
+        for (int row = 0; row < 8; row++) {
+            if (value & 0x80) {
+                currentRows[row] |= 1 << (7 - column);
+            } else {
+                currentRows[row] &= ~(1 << (7 - column));
+            }
+            value <<= 1;
+        }
+
+        for (int row = 0; row < 8; row++) {
+            spiSendCommand(row + 1, currentRows[row]);
+        }
+    }
+}
+
+void MAX7219LEDMatrix::set(int row, int column, bool on) {
+    if ((row >= 0) && (row <= 7)) {
+        if ((column >= 0) && (column <= 7)) {
+            if (on) {
+                currentRows[row] |= 1 << (7 - column);
+            } else {
+                currentRows[row] &= ~(1 << (7 - column));
+            }
+
+            for (int row = 0; row < 8; row++) {
+                spiSendCommand(row + 1, currentRows[row]);
+            }
+        }
+    }
+}
+
+void MAX7219LEDMatrix::clearDisplay() {
+    for (int row = 1; row <= 8; row++) {
+        currentRows[row] = 0x00;
+        spiSendCommand(row, 0x00);
+    }
+}
+
+void MAX7219LEDMatrix::configure() {
+    // DISPLAY TEST MODE: 0 (OFF)
+    spiSendCommand(Command_TestMode, 0x00);
+
+    // SCAN LIMIT: 7
+    spiSendCommand(Command_ScanLimit, 0x07);
+
+    // DECODE: No Decode
+    spiSendCommand(Command_DecodeMode, 0x00);
+
+    // Mode: SHUTDOWN
+    spiSendCommand(Command_Shutdown, 0x00);
+}
+
+void MAX7219LEDMatrix::spiSendCommand(uint8_t command, uint8_t data) {
+    digitalWrite(csPin, LOW);
+
+    shiftOut(dinPin, clkPin, MSBFIRST, command);
+    shiftOut(dinPin, clkPin, MSBFIRST, data);
+    
+    digitalWrite(csPin, HIGH);
+}
---
+++ Personal-Libraries/MAX7219 LED Matrix/MAX7219LEDMatrix.h
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+
+class MAX7219LEDMatrix {
+    public:
+        MAX7219LEDMatrix(int din, int cs, int clk);
+
+        MAX7219LEDMatrix(const MAX7219LEDMatrix &) = delete;
+        MAX7219LEDMatrix(MAX7219LEDMatrix &&) = delete;
+
+        MAX7219LEDMatrix & operator=(MAX7219LEDMatrix &&) = delete;
+        MAX7219LEDMatrix & operator=(const MAX7219LEDMatrix &) = delete;
+
+    public:
+        void initialize();
+        void shutdown(bool on);
+        void displayTest(bool on);
+
+        // Minimum 0x00 - Max 0x0F - All vaoues in between available
+        void setBrightness(uint8_t level);
+
+        void setRow(int row, uint8_t value);
+        void setColumn(int column, uint8_t value);
+        void set(int row, int column, bool on);
+        void clearDisplay();
+
+    private:
+        void configure();
+        void spiSendCommand(uint8_t command, uint8_t data);
+
+    private:
+        int dinPin;
+        int csPin;
+        int clkPin;
+
+        uint8_t currentRows[8] = { 0x00 };
+
+    private:
+        static const uint8_t Command_TestMode = 0x0F;
+        static const uint8_t Command_Shutdown = 0x0C;
+        static const uint8_t Command_ScanLimit = 0x0B;
+        static const uint8_t Command_Intensity = 0x0A;
+        static const uint8_t Command_DecodeMode = 0x09;
+};
