504f35aacea6039730bca2368d19cd7f58af88e5
Gilles Zunino <gzunino@hotmail.com>
2022-12-22 22:53:37 -0800

    Add "Gyroscope - Accelerometer with MPU6050'

---
+++ Gyroscope - Accelerometer with MPU6050/src/main.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+#include <avr8-stubs-utilities.h>
+
+
+//
+// The module is I2C - Connect SDA (typically A4) and SCL (typically A5)
+// The module is powered with 3.3V - 5V
+//
+
+Adafruit_MPU6050 mpu;
+
+void setup() {
+  INIT_DEBUGGER();
+
+  Serial.begin(9600);
+
+  Serial.println("Initializing MPU6050");
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // Setup motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+}
+
+void loop() {
+  // Get new sensor events with the readings
+  if (mpu.getMotionInterruptStatus()) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    Serial.print("AccelX:");
+    Serial.print(a.acceleration.x);
+    Serial.print(",");
+    Serial.print("AccelY:");
+    Serial.print(a.acceleration.y);
+    Serial.print(",");
+    Serial.print("AccelZ:");
+    Serial.print(a.acceleration.z);
+    Serial.print(", ");
+    Serial.print("GyroX:");
+    Serial.print(g.gyro.x);
+    Serial.print(",");
+    Serial.print("GyroY:");
+    Serial.print(g.gyro.y);
+    Serial.print(",");
+    Serial.print("GyroZ:");
+    Serial.print(g.gyro.z);
+    Serial.print(",");
+    Serial.print("Temp:");
+    Serial.print(temp.temperature);
+    Serial.println("");
+  }
+
+  delay(10);
+}
